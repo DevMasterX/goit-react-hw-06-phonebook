295d5bf4c06879ac18fa96808a9cf41a609b6e11
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2023-10-17 19:16:32 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ContactListWrapper = styled('ul')(() => {
+  return {
+    listStyle: 'none',
+    paddingLeft: '15px',
+    marginBottom: '20px',
+  };
+});
